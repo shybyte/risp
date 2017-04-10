ff7c6c3e10d30820280df91f94e6c5f71a11207b
shybyte <ein@volloeko.de>
2017-04-10 07:40:16 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate regex;
+
+mod parse;
+mod types;
+
+use std::fs::File;
+use std::io::prelude::*;
+use parse::parse;
+
+
+fn main() {
+    let mut file = File::open("examples/simple.risp").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let wrapped_content = "(".to_string() + &contents + ")";
+
+    let ast = parse(&wrapped_content);
+    println!("{:?}", ast);
+}
+
